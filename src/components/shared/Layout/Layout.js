85e930d0862dddffb7011386d817b9b629a59da9
Chadd Poggenpoel <chaddpoggenpoel@gmail.com>
2021-02-16 15:50:50 +0200

    0.0.1

--- src/components/shared/Layout/Layout.js
+++ src/components/shared/Layout/Layout.js
@@ -1,11 +1,9 @@
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-import Alert from "../components/alert";
 
 const Layout = ({ children }) => {
   return (
     <div className="site">
-      <Alert preview={preview} />
       <Header />
       <main className="site-content">{children}</main>
       <Footer />
