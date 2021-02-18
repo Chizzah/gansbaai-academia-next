c59a727334b84d56bf96ce1035d523463c5b172e
Chadd Poggenpoel <chaddpoggenpoel@gmail.com>
2021-02-18 11:29:30 +0200

    0.0.1

--- src/pages/articles/[slug].js
+++ src/pages/articles/[slug].js
@@ -6,22 +6,20 @@ import { client } from "../../utils/contentfulClient";
 
 const { BLOCKS } = require("@contentful/rich-text-types");
 
-export async function getStaticPaths() {
+export const getStaticPaths = async () => {
   let data = await client.getEntries({
     content_type: "news",
   });
 
-  console.log(data);
-
   return {
-    paths: data?.items.map((item) => ({
+    paths: data.items.map((item) => ({
       params: { slug: item.fields.slug },
     })),
     fallback: true,
   };
-}
+};
 
-export async function getStaticProps({ params }) {
+export const getStaticProps = async ({ params }) => {
   let data = await client.getEntries({
     content_type: "news",
     "fields.slug": params.slug,
@@ -33,64 +31,60 @@ export async function getStaticProps({ params }) {
     },
     revalidate: 60,
   };
-}
+};
 
 const Article = ({ article }) => {
-  console.log(article);
+  // console.log(article);
   return (
     <Layout>
       <article className="container-fluid bg-light">
         <div className="container py-5">
-          {/* <div className="row">
+          <div className="row">
             <div className="col">
               <Image
-                src={"https:" + article.fields.featuredImage.fields.file.url}
-                alt={article.fields.featuredImage.fields.file.title}
+                src={`https://${article?.fields.image.fields.file.url}`}
+                alt={article?.fields.image.fields.file.title}
                 layout="responsive"
                 objectFit="cover"
-                width={
-                  article.fields.featuredImage.fields.file.details.image.width
-                }
-                height={
-                  article.fields.featuredImage.fields.file.details.image.height
-                }
+                width={article?.fields.image.fields.file.details.image.width}
+                height={article?.fields.image.fields.file.details.image.height}
               />
             </div>
-          </div> */}
+          </div>
           <div className="row py-5">
             <div className="col">
               <p className="mb-0 text-muted text-uppercase">
-                {article.fields.date}
+                {article?.fields.date}
               </p>
-              <h1>{article.fields.title}</h1>
+              <h1>{article?.fields.title}</h1>
               <p>
-                by{" "}
+                from{" "}
                 <span className="text-muted fw-bold">
-                  {article.fields.author.fields.name}
+                  {article?.fields.author.fields.name}
                 </span>
               </p>
-              <p style={{ maxWidth: "120ch" }}>{article.fields.summary}</p>
+              <p style={{ maxWidth: "120ch" }}>{article?.fields.summary}</p>
             </div>
           </div>
-          {/* <div className="row">
+          <div className="row">
             <div className="col">
-              {documentToReactComponents(article.fields.content, {
+              {documentToReactComponents(article?.fields.content, {
                 renderNode: {
                   [BLOCKS.EMBEDDED_ASSET]: (node) => (
                     <Image
-                      src={"https:" + node.data.target.fields.file.url}
-                      width={node.data.target.fields.file.details.image.width}
-                      height={node.data.target.fields.file.details.image.height}
+                      src={`https://${node.data.target?.fields.file.url}`}
+                      width={node.data.target?.fields.file.details.image.width}
+                      height={
+                        node.data.target?.fields.file.details.image.height
+                      }
                     />
                   ),
                 },
               })}
             </div>
-          </div> */}
+          </div>
         </div>
       </article>
     </Layout>
   );
 };
-
-export default Article;
